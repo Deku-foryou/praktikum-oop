4001d83480c5fa144246dab552efa72db77375dc
Deku-foryou <reyhanmagribi57@gmail.com>
2025-12-08 15:54:59 +0700

    "Praktikum I: fluent interface"

---
+++ praktikum-2/builder.php
@@ -0,0 +1,20 @@
+<?php
+class Builder {
+private string $text = "";
+public function add(string $s): self {
+$this->text .= $s;
+return $this;
+}
+
+public function upper(): self {
+$this->text = strtoupper($this->text);
+return $this;
+}
+public function get(): string {
+return $this->text;
+
+}
+}
+
+$b = (new Builder())->add("Halo ")->add("Dunia");
+echo (new Builder())->add("halo ")->add("dunia")->upper()->get();
