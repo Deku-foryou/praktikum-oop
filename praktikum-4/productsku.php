0f1ac975ed25f9a69f9a5d3feb165132e3644464
Deku-foryou <reyhanmagribi57@gmail.com>
2025-12-09 12:11:42 +0700

    Praktikum D — Read-Only Property (Tanpa Setter)

---
+++ praktikum-4/productsku.php
@@ -0,0 +1,18 @@
+<?php
+
+class Product {
+    private string $sku;
+
+    public function __construct(string $sku) {
+        $this->sku = $sku;
+    }
+
+    public function getSku(): string {
+        return $this->sku;
+    }
+}
+
+$p = new Product("SKU-001");
+$p->sku = "SKU-XYZ";
+
+echo $p->getSku();
